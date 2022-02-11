eb68cb0e374c0693e167d82e73d21bb17dfe6a40
minhchi2494 <minhchi2494@gmail.com>
2022-02-11 10:56:57 +0700

    Ma hoa admin

---
+++ sales_management_mobile_app/WebAPI/Services/AdminServices.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+using WebAPI.Repository;
+
+using WebAPI.Models;
+
+namespace WebAPI.Services
+{
+    public class AdminServices : IAdminServices
+    {
+        private readonly Project4Context _context;
+
+        public AdminServices(Project4Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<Admin> checkLogin(string username, string password)
+        {
+            var model = _context.Admins.SingleOrDefault(a => a.Username.Equals(username));
+            if (model != null)
+            {
+                string pass = PinCodeSecurity.pinDecrypt(model.Password);
+                if (password.Equals(pass))
+                {
+                    return model;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        //public async Task<Admin> checkLogin(Admin admin)
+        //{
+        //    var model = _context.Admins.SingleOrDefault(a => a.Username.Equals(admin.Username));
+        //    if (model != null)
+        //    {
+        //        string pass = PinCodeSecurity.pinDecrypt(model.Password);
+        //        if (admin.Password.Equals(pass))
+        //        {
+        //            return model;
+        //        }
+        //        else
+        //        {
+        //            return null;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return null;
+        //    }
+        //}
+
+        public async Task<bool> createAdmin(Admin newAdmin)
+        {
+            var model = _context.Admins.SingleOrDefault(x=>x.Id.Equals(newAdmin.Id));
+            if (model == null)
+            {
+                newAdmin.Password = PinCodeSecurity.pinEncrypt(newAdmin.Password);
+                _context.Admins.Add(newAdmin);
+                _context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Task<bool> updateAdmin(Admin editAdmin)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
