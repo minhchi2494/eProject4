2e535034b8aefbb72c2035b0f8b2b0026707c437
minhchi2494 <minhchi2494@gmail.com>
2022-03-12 17:23:55 +0700

    Add icon

--- sales_management_mobile_app/WebAPI/Models/Product.cs
+++ sales_management_mobile_app/WebAPI/Models/Product.cs
@@ -22,8 +22,8 @@ namespace WebAPI.Models
         public string Unit { get; set; }
         public string Images { get; set; }
 
-        [NotMapped]
-        public IFormFile ImageFiles { get; set; }
+        //[NotMapped]
+        //public IFormFile ImageFiles { get; set; }
 
         public bool? IsActive { get; set; }
 
