1f62acaeadf5d6c82e6873945884ee54ff92c9eb
minhchi2494 <minhchi2494@gmail.com>
2022-03-16 18:56:31 +0700

    Change in model  StoreSalesDetail Saledetail

--- sales_management_mobile_app/WebAPI/Models/SalesDetail.cs
+++ sales_management_mobile_app/WebAPI/Models/SalesDetail.cs
@@ -7,10 +7,10 @@ namespace WebAPI.Models
 {
     public partial class SalesDetail
     {
-        public SalesDetail()
-        {
-            StoreSalesDetails = new HashSet<StoreSalesDetail>();
-        }
+        //public SalesDetail()
+        //{
+        //    StoreSalesDetails = new HashSet<StoreSalesDetail>();
+        //}
 
         public int Id { get; set; }
         public int? SalesActualQuantity { get; set; }
@@ -19,8 +19,9 @@ namespace WebAPI.Models
         //public decimal? Price { get; set; }
         public DateTime? Date { get; set; }
         public int UserId { set; get; }
+        public virtual User User { get; set; }
         public virtual Product Product { get; set; }
         public virtual Target Target { get; set; }
-        public virtual ICollection<StoreSalesDetail> StoreSalesDetails { get; set; }
+        //public virtual ICollection<StoreSalesDetail> StoreSalesDetails { get; set; }
     }
 }
