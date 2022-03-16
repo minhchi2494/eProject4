509183f080036e318d90c023c97b98d01d10cedb
minhchi2494 <minhchi2494@gmail.com>
2022-03-17 02:57:55 +0700

    Add vStoreUser on Blazor UI

--- sales_management_mobile_app/BlazorApp/Program.cs
+++ sales_management_mobile_app/BlazorApp/Program.cs
@@ -51,6 +51,8 @@ namespace BlazorApp
 
             builder.Services.AddScoped<ITargetUserManagerServices, TargetUserManagerServices>();
 
+            builder.Services.AddScoped<IStoreUserServices, StoreUserServices>();
+
             builder.Services.AddAuthorizationCore();
 
 
---
+++ sales_management_mobile_app/BlazorApp/Services/IStoreUserServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorApp.Models;
+
+namespace BlazorApp.Services
+{
+    public interface IStoreUserServices
+    {
+        Task<List<vStoreUser>> getStoreUsers(int id);
+    }
+}
