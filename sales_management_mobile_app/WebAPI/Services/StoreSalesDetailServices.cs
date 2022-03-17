378cc3e30609431d12356006abd7a0fcb67dc376
minhchi2494 <minhchi2494@gmail.com>
2022-03-17 13:14:59 +0700

    Add new

--- sales_management_mobile_app/WebAPI/Services/StoreSalesDetailServices.cs
+++ sales_management_mobile_app/WebAPI/Services/StoreSalesDetailServices.cs
@@ -39,10 +39,14 @@ namespace WebAPI.Services
             //var result = _context.StoreSalesDetails.Include(x => x.Product).Include(x => x.SalesDetail).Include(x => x.Store).
             //            First(x => x.StoreId.Equals(storeId));
             var result = _context.StoreSalesDetails.Include(x => x.Product).Include(x => x.Store).Include(x => x.User).
-            First(x => x.StoreId.Equals(storeId));
-
+                        First(x => x.StoreId.Equals(storeId));
+            //check daily sale
             var acc = _context.SalesDetails.Where(x => x.UserId.Equals(ssd.UserId)).ToList();
-            var temp = acc.Where(x=>x.ProductId.Equals(ssd.ProductId)).ToList();
+            var temp = acc.Where(x => x.ProductId.Equals(ssd.ProductId)).ToList();
+
+            //check target
+            var salestarget = _context.Targets.Where(x => x.Id.Equals(ssd.User.TargetId));
+
 
             int quant = 0;
             foreach (var item in temp)
@@ -54,29 +58,29 @@ namespace WebAPI.Services
             var date = temp.SingleOrDefault(x => x.Date.Equals(ssd.Date));
 
             SalesDetail bien = new SalesDetail();
-            //SalesDetail acc = new SalesDetail();
             if (result != null)
             {
                 _context.StoreSalesDetails.Add(ssd);
                 _context.SaveChanges();
 
-                if (temp.Count()!=0)
+                if (temp.Count() != 0)
                 {
-                        if (date !=null)
-                                        {
-                                            date.SalesActualQuantity = ssd.StoreActualQuantity + quant;
-                                            date.TargetId = date.TargetId;
-                                            date.UserId = ssd.UserId;
-                                            date.Date = ssd.Date;
-                                            date.ProductId = ssd.ProductId;
+                    if (date != null)
+                    {
+                        date.SalesActualQuantity = ssd.StoreActualQuantity + quant;
+                        date.TargetId = date.TargetId;
+                        date.UserId = ssd.UserId;
+                        date.Date = ssd.Date;
+                        date.ProductId = ssd.ProductId;
                         _context.SaveChanges();
                     }
-                                        else { 
-                                            bien.SalesActualQuantity = ssd.StoreActualQuantity;
-                                            bien.TargetId = ssd.User.TargetId;
-                                            bien.UserId = ssd.UserId;
-                                            bien.Date = ssd.Date;
-                                            bien.ProductId = ssd.ProductId;
+                    else
+                    {
+                        bien.SalesActualQuantity = ssd.StoreActualQuantity;
+                        bien.TargetId = ssd.User.TargetId;
+                        bien.UserId = ssd.UserId;
+                        bien.Date = ssd.Date;
+                        bien.ProductId = ssd.ProductId;
                         _context.SaveChanges();
                     }
 
@@ -88,6 +92,7 @@ namespace WebAPI.Services
                     bien.UserId = ssd.UserId;
                     bien.Date = ssd.Date;
                     bien.ProductId = ssd.ProductId;
+                    _context.SalesDetails.Add(bien);
                     _context.SaveChanges();
                 }
                 return true;
