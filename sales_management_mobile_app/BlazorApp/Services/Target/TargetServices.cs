ec1a595a85cd648c3145c4a99e8420146a584a61
minhchi2494 <minhchi2494@gmail.com>
2022-03-28 22:56:15 +0700

    Add minor change

--- sales_management_mobile_app/BlazorApp/Services/Target/TargetServices.cs
+++ sales_management_mobile_app/BlazorApp/Services/Target/TargetServices.cs
@@ -28,7 +28,7 @@ namespace BlazorApp.Services
         public async Task<bool> createTarget(Target newTarget)
         {
             var result = await _httpClient.PostAsJsonAsync($"/api/Target?Targets={newTarget.Targets}&FromDate={newTarget.FromDate}" +
-                $"&ToDate={newTarget.ToDate}&ActualQuantity={newTarget.ActualQuantity}&CreatedOn={newTarget.CreatedOn}", newTarget);
+                $"&ToDate={newTarget.ToDate}&CreatedOn={newTarget.CreatedOn}&UserId={newTarget.UserId}", newTarget);
             if (result.IsSuccessStatusCode)
             {
                 return true;
