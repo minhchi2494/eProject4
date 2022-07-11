4a1e787385ec1ff21505451097f94e5fee66d7fc
minhchi2494 <minhchi2494@gmail.com>
2022-07-11 11:30:59 +0700

    Fix function create manager blazor and minor fix in api

--- sales_management_mobile_app/BlazorApp/Models/Manager.cs
+++ sales_management_mobile_app/BlazorApp/Models/Manager.cs
@@ -8,28 +8,39 @@ namespace BlazorApp.Models
 {
     public partial class Manager
     {
-        [Required]
+        [Required(ErrorMessage = "Id is required")]
+        [StringLength(5, MinimumLength = 2, ErrorMessage = "Id must contains from 2 to 5 characters!")]
         public string Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Username must contains from 2 to 20 characters!")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Password must contains from 3 to 20 characters!")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Fullname is required")]
+        [StringLength(40, MinimumLength = 2, ErrorMessage = "Fullname must contains from 2 to 40 characters!")]
         public string Fullname { get; set; }
 
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Phone is required")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Phone must contains from 8 to 20 characters!")]
         public string Phone { get; set; }
+
         public string Address { get; set; }
 
         [Required]
         public string DirectorId { get; set; }
 
 
-        public int KpiValue { get; set; }
-        public int ActualKpi { get; set; }
+        public int KpiValue { get; set; } = 0;
+        public int ActualKpi { get; set; } = 0;
 
         public virtual Director Director { get; set; }
         public virtual ICollection<User> Users { get; set; }
