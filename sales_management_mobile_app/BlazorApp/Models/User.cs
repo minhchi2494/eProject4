167b9e86e344a32b543edd50ac366b15e06abf98
minhchi2494 <minhchi2494@gmail.com>
2022-07-12 10:05:14 +0700

    Add account ui

--- sales_management_mobile_app/BlazorApp/Models/User.cs
+++ sales_management_mobile_app/BlazorApp/Models/User.cs
@@ -8,29 +8,39 @@ namespace BlazorApp.Models
 {
     public partial class User
     {
+        [Required(ErrorMessage = "Id is required")]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Username must contains from 2 to 20 characters!")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Password must contains from 3 to 20 characters!")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Fullname is required")]
+        [StringLength(40, MinimumLength = 2, ErrorMessage = "Fullname must contains from 2 to 40 characters!")]
         public string Fullname { get; set; }
 
+
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Phone must contains from 8 to 20 characters!")]
         public string Phone { get; set; }
+
         public string Address { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Manager Id is required")]
         public string ManagerId { get; set; }
 
-        public int KpiValue { get; set; }
-        public int ActualKpi { get; set; }
+        public int KpiValue { get; set; } = 0;
+        public int ActualKpi { get; set; } = 0;
 
-        public bool? IsActive { get; set; }
+        public bool? IsActive { get; set; } = true;
 
 
         public virtual Manager Manager { get; set; }
