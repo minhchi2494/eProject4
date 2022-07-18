7b3e4e614c60cae32d4af3454dfd37827b149dd2
minhchi2494 <minhchi2494@gmail.com>
2022-07-18 22:43:16 +0700

    Add download function for report Blazor

---
+++ sales_management_mobile_app/WebAPI/Controllers/ManagerReportExportController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagerReportExportController : ControllerBase
+    {
+        private ManagerReportExportServices _service;
+        private string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        private string fileName = "ManagerReport.xlsx";
+
+        public ManagerReportExportController(ManagerReportExportServices service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult DownloadExport()
+        {
+            return File(_service.CreateFullExport(), contentType, fileName);
+        }
+    }
+}
